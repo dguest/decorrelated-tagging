25c24c90b56dcc5f8caf73664fe551eddd4cc005
Dan Guest <dguest@cern.ch>
2017-04-02 18:59:12 +0200

    add configuration data files

--- src/dtf-main.cxx
+++ src/dtf-main.cxx
@@ -5,18 +5,19 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <cmath>
 
 typedef std::map<std::string, std::map<std::string, double> > input_t;
 
 input_t dummy_feature_map() {
   return {
     {"inputs", {
-        {"jet_pt", 1},
-        {"jet_m", 1},
+        {"jet_pt_log1p", std::log1p(1)},
+        {"jet_m_log1p", 1},
         {"jet_eta", 1},
         {"jet_phi", 1},
-        {"photon_E", 1},
-        {"photon_pt", 1},
+        {"photon_E_log1p", std::log1p(1)},
+        {"photon_pt_log1p", std::log1p(1)},
         {"photon_eta", 1},
         {"photon_phi", 1},
         {"jet_tau21", 1},
@@ -27,9 +28,9 @@ input_t dummy_feature_map() {
 
 int main(int argc, char* argv[]) {
   using namespace lwt;
-  std::ifstream input("data/dtf.json");;
+  std::ifstream input("data/lwtnn-dtf-config.json");;
   LightweightGraph graph(parse_json_graph(input));
   auto outmap = graph.compute(dummy_feature_map());
-  std::cout << outmap.at("out_0") << std::endl;
+  std::cout << outmap.at("discriminant") << std::endl;
   return 0;
 }
